abafd705646d1a6a0d00f0ca954744f046819cb1
ju4n_0 <juanswc87@gmail.com>
2025-11-14 13:55:47 -0300

    Entidad Empleado Core/Adapter completa

--- src/main/java/com/olivera/sistema_reparacion/infrastucture/adapaters/controllers/EmpleadoController.java
+++ src/main/java/com/olivera/sistema_reparacion/infrastucture/adapaters/controllers/EmpleadoController.java
@@ -1,10 +1,10 @@
 package com.olivera.sistema_reparacion.infrastucture.adapaters.controllers;
 
+import com.olivera.sistema_reparacion.application.dto.empleado.ActualizarEmpleadoCommand;
 import com.olivera.sistema_reparacion.application.dto.empleado.EmpleadoResponse;
 import com.olivera.sistema_reparacion.application.dto.empleado.RegistrarEmpleadoCommand;
-import com.olivera.sistema_reparacion.application.ports.in.empleado.BuscarEmpleadoPorId;
-import com.olivera.sistema_reparacion.application.ports.in.empleado.ListarTodosLosEmpleados;
-import com.olivera.sistema_reparacion.application.ports.in.empleado.RegistrarEmpleado;
+import com.olivera.sistema_reparacion.application.dto.reparacion.ReparacionResponse;
+import com.olivera.sistema_reparacion.application.ports.in.empleado.*;
 import com.olivera.sistema_reparacion.domain.entities.Empleado;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +19,21 @@ public class EmpleadoController {
     private final RegistrarEmpleado registrarEmpleado;
     private final BuscarEmpleadoPorId buscarEmpleadoPorId;
     private final ListarTodosLosEmpleados listarTodosLosEmpleados;
+    private final EliminarEmpleado eliminarEmpleado;
+    private final VerificarSiExisteEmpleadoPorId verificarSiExisteEmpleadoPorId;
+    private final VerReparacionesAsignadas verReparacionesAsignadas;
+    private final ActualizarInformacionEmpleado actualizarInformacionEmpleado;
 
     public EmpleadoController(RegistrarEmpleado registrarEmpleado,
                               BuscarEmpleadoPorId buscarEmpleadoPorId,
-                              ListarTodosLosEmpleados listarTodosLosEmpleados) {
+                              ListarTodosLosEmpleados listarTodosLosEmpleados, EliminarEmpleado eliminarEmpleado, VerificarSiExisteEmpleadoPorId verificarSiExisteEmpleadoPorId, VerReparacionesAsignadas verReparacionesAsignadas, ActualizarInformacionEmpleado actualizarInformacionEmpleado) {
         this.registrarEmpleado = registrarEmpleado;
         this.buscarEmpleadoPorId = buscarEmpleadoPorId;
         this.listarTodosLosEmpleados = listarTodosLosEmpleados;
+        this.eliminarEmpleado = eliminarEmpleado;
+        this.verificarSiExisteEmpleadoPorId = verificarSiExisteEmpleadoPorId;
+        this.verReparacionesAsignadas = verReparacionesAsignadas;
+        this.actualizarInformacionEmpleado = actualizarInformacionEmpleado;
     }
 
     @PostMapping
@@ -42,7 +50,7 @@ public class EmpleadoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EmpleadoResponse> buscarPorId(@PathVariable Long id) {
+    public ResponseEntity<EmpleadoResponse> buscarPorId(@PathVariable("id") Long id) {
         EmpleadoResponse response = buscarEmpleadoPorId.buscarEmpleadoPorId(id);
         return ResponseEntity.ok(response);
     }
@@ -52,5 +60,31 @@ public class EmpleadoController {
         List<EmpleadoResponse> response = listarTodosLosEmpleados.listarTodosLosEmpleados();
         return ResponseEntity.ok(response);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmpleadoResponse> actualizar(@PathVariable Long id, @RequestBody Empleado request){
+        ActualizarEmpleadoCommand command = new ActualizarEmpleadoCommand(request.getId(),
+                request.getNombre(),
+                request.getApellido(),
+                request.getEspecialidad(),
+                request.getEmail()
+        );
+        EmpleadoResponse nuevo = actualizarInformacionEmpleado.actualizarEmpleado(command);
+        return ResponseEntity.ok(nuevo);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminar(@PathVariable Long id){
+        eliminarEmpleado.eliminarEmpleado(id);
+        return ResponseEntity.ok("Empleado de id "+id+" eliminado correctamente");
+    }
+
+    //relaciones prueba
+
+    @GetMapping("/{id}/reparaciones")
+    public ResponseEntity<List<ReparacionResponse>> verReparacionesPorId(@PathVariable("id") Long empleadoId){
+        List<ReparacionResponse> reparaciones = verReparacionesAsignadas.reparacionesAsignadasPorId(empleadoId);
+        return ResponseEntity.ok(reparaciones);
+    }
+
 }
 //por favor Dios q ande xd
