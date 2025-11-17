292c4273a8f6e621fbb884a2f964ecc8ef3f8b6c
ju4n_0 <juanswc87@gmail.com>
2025-11-17 01:10:32 -0300

    Error solucionado - 40 test corriendo hasta el momento

--- src/test/java/com/olivera/sistema_reparacion/infrastructure/TestEmpleadoController.java
+++ src/test/java/com/olivera/sistema_reparacion/infrastructure/TestEmpleadoController.java
@@ -3,6 +3,8 @@ package com.olivera.sistema_reparacion.infrastructure;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.olivera.sistema_reparacion.application.dto.empleado.EmpleadoResponse;
 import com.olivera.sistema_reparacion.application.ports.in.empleado.*;
+import com.olivera.sistema_reparacion.domain.exceptions.DatosNoValidosException;
+import com.olivera.sistema_reparacion.domain.exceptions.empleado.EmpleadoNoEncontradoException;
 import com.olivera.sistema_reparacion.domain.exceptions.empleado.EmpleadoYaExisteException;
 import com.olivera.sistema_reparacion.infrastucture.adapaters.controllers.EmpleadoController;
 import com.olivera.sistema_reparacion.infrastucture.config.GlobalExceptionHandler;
@@ -14,11 +16,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*; //fallo de importaciones, importaba lo que quería para los test GET
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(EmpleadoController.class)
 public class TestEmpleadoController {
@@ -45,7 +51,7 @@ public class TestEmpleadoController {
 
     //Test Post
     @Test
-    void TestRegistraEmpleadoYDevuelveStatus201() throws Exception {
+    void testRegistraEmpleadoYDevuelveStatus201() throws Exception {
         String requestBody = """
             {
                 "nombre": "Juan",
@@ -71,7 +77,7 @@ public class TestEmpleadoController {
         verify(registrarEmpleado, times(1)).saveEmpleado(any());
     }
     @Test
-    void TestNoDebeRegistrarEmailDuplicado() throws Exception {
+    void testNoDebeRegistrarEmailDuplicado() throws Exception {
         String requestBody = """
             {
                 "nombre": "Juan",
@@ -88,7 +94,7 @@ public class TestEmpleadoController {
         verify(registrarEmpleado, times(1)).saveEmpleado(any());
     }
     @Test
-    void TestNoDebeRegistrarPorDatosInvalido() throws Exception {
+    void testNoDebeRegistrarPorDatosInvalido() throws Exception {
         String requestBody = """
             {
                 "nombre": "Juan",
@@ -97,12 +103,91 @@ public class TestEmpleadoController {
                 "email": "juancom"
             }
         """;
-        when(registrarEmpleado.saveEmpleado(any())).thenThrow(new EmpleadoYaExisteException("El email juan@gmail.com ya esta registrado"));
+        when(registrarEmpleado.saveEmpleado(any())).thenThrow(new DatosNoValidosException("Datos no validos"));
         mockMvc.perform(post("/api/empleados")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestBody))
                 .andExpect(status().isBadRequest()); //seria el 400
         verify(registrarEmpleado, times(1)).saveEmpleado(any());
     }
+    //GET
+    @Test
+    void testDeberiaListarTodosLosEmpleadosYDevolverStatus200() throws Exception {
+        List<EmpleadoResponse> empleados = Arrays.asList(
+                new EmpleadoResponse(1L, "Juan", "Olivera", "Programador", "juan@gmail.com"),
+                new EmpleadoResponse(2L, "María", "López", "Diseñadora", "maria@gmail.com"),
+                new EmpleadoResponse(3L, "Pedro", "García", "Tester", "pedro@gmail.com")
+        );
+        when(listarTodosLosEmpleados.listarTodosLosEmpleados()).thenReturn(empleados);
+
+        mockMvc.perform(get("/api/empleados"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].nombre").value("Juan")) //[indice].atributo?-> valor esperado
+                .andExpect(jsonPath("$[1].nombre").value("María"))
+                .andExpect(jsonPath("$[2].nombre").value("Pedro"));
+
+        verify(listarTodosLosEmpleados, times(1)).listarTodosLosEmpleados();
+    }
+    @Test
+    void testDeberiaEncontrarAlEmpleadoPorIdYDevolver200() throws Exception {
+        String requestBody = """
+            {
+                "nombre": "Juan",
+                "apellido": "Olivera",
+                "especialidad": "Programador",
+                "email": "juancom"
+            }
+        """;
+        EmpleadoResponse response= new  EmpleadoResponse(1L, "Juan", "Olivera", "Programador", "juan@gmail.com");
+        when(buscarEmpleadoPorId.buscarEmpleadoPorId(1L)).thenReturn(response);
+
+        mockMvc.perform(get("/api/empleados/{id}", 1L))
+                .andExpect(status().isOk());
+    }
+    //delete
+    @Test
+    void testDeberiaEliminarEmpleadoYDevolverStatus200() throws Exception {
+        Long empleadoId = 1L;
+        doNothing().when(eliminarEmpleado).eliminarEmpleado(empleadoId);
+
+        mockMvc.perform(delete("/api/empleados/{id}", empleadoId))
+                .andExpect(status().isOk()); //200 pq asi lo puse yo
+
+        verify(eliminarEmpleado, times(1)).eliminarEmpleado(empleadoId);
+    }
+    //put
+    @Test
+    void testDeberiaActualizarEmpleadoYDevolverStatus200() throws Exception {
+
+        Long empleadoId = 1L;
+        String requestBody = """
+            {
+                "nombre": "Juan Carlos",
+                "apellido": "Olivera",
+                "especialidad": "Senior Developer",
+                "email": "juancarlos@gmail.com"
+            }
+        """;
+
+        EmpleadoResponse response = new EmpleadoResponse(
+                1L, "Juan Carlos", "Olivera", "Senior Developer", "juancarlos@gmail.com"
+        );
+
+        when(actualizarInformacionEmpleado.actualizarEmpleado(any())).thenReturn(response);
+
+        mockMvc.perform(put("/api/empleados/{id}", empleadoId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.nombre").value("Juan Carlos"))
+                .andExpect(jsonPath("$.especialidad").value("Senior Developer"))
+                .andExpect(jsonPath("$.email").value("juancarlos@gmail.com"));
+
+        verify(actualizarInformacionEmpleado, times(1)).actualizarEmpleado(any());
+    }
 }
 //nota CONECTAR INICIAR SPRING BOOT O EXPLOTA EL TEST
+//NOTA 2: PONER BIEN LAS EXCEPCIONES
+//test put y delete hechos con ia
